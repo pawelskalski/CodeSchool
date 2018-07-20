386c6cabd980514521dee4541763c837f876be1b
pawel <pawel@DELL.home>
2018-07-20 14:38:35 +0200

    initializing

---
+++ src/pl/coderslab/dao/UserGroupDao.java
@@ -0,0 +1,16 @@
+package pl.coderslab.dao;
+
+public class UserGroupDao {
+
+
+//    public void save(UserGroup user){
+//
+//    }
+//
+//    public List<UserGroup> findAll(){
+//
+//    }
+
+
+
+}
